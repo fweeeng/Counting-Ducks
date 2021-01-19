08cd6758cbefbb5548c7bdc4c75a1b2633bba162
fweeeng <91084706@g.mvusd.net>
2021-01-19 20:24:36 +0000

    Added text field and scuba duck image

--- Main.java
+++ Main.java
@@ -33,12 +33,21 @@ public class Main
     });
     //=========Label 1===========
     JLabel label1 = new JLabel("Count the ducks");
+    JTextField textfield1 = new JTextField(10);
 
     panel1.add(button1);
     panel1.add(button2);
     panel1.add(label1);
+    panel1.add(textfield1);
 
-    frame1.getContentPane().add(panel1);
-    
+    //=========Panel 2==========
+    JPanel panel2 = new JPanel(new BorderLayout(10,10));
+    //=========Image 1 - the DUCK========
+    ImageIcon duckImg = new ImageIcon("duckicon.jpg", "A little scuba duck");
+    JLabel duckLabelPic = new JLabel(duckImg);
+    panel2.add(duckLabelPic);
+
+    frame1.getContentPane().add(BorderLayout.NORTH,panel1);
+    frame1.getContentPane().add(BorderLayout.CENTER, panel2);
   }//end main method
 }//end class Main
