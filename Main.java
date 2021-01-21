bd56ed52761b70dacbfb33f6537610a8d46fa4a7
fweeeng <91084706@g.mvusd.net>
2021-01-21 19:21:37 +0000

    Button1 randomoly changes the output of label1

--- Main.java
+++ Main.java
@@ -6,20 +6,28 @@ public class Main
 {
   public static void main(String args[])
   {
-    JFrame frame1 = new JFrame("This is a JFrame");
+    JFrame frame1 = new JFrame("This is a Real Duck Counter");
     frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //MUST HAVE THIS LINE
     frame1.setSize(500,500);
     frame1.setVisible(true);
 
     //========Panel to hold the buttons=========
     JPanel panel1 = new JPanel(new GridLayout(2, 2, 20, 20));
+
+    //=========Label 1===========
+    JLabel label1 = new JLabel("Count the ducks");
+    JTextField textfield1 = new JTextField(10);
+
     //========Button 1=========
     JButton button1 = new JButton("Click me!");
     button1.addActionListener(new ActionListener()
     {
       public void actionPerformed(ActionEvent e)
       {
+        String[] phrases = {"Counting ducks", "Look out for scuba duck!", "Gotta count them all!", "You're an expert duck counter"};
+        String newText = phrases[(int)(Math.random()*phrases.length)];
         System.out.println("Yay! You clicked the button!");
+        label1.setText(newText);
       }//end the block of what the button will do
     });//end the ActionListener
     //---------Button 2-----------
@@ -31,9 +39,7 @@ public class Main
       System.out.println("Thanks, pal!");
       }//end of the button block
     });
-    //=========Label 1===========
-    JLabel label1 = new JLabel("Count the ducks");
-    JTextField textfield1 = new JTextField(10);
+    
 
     panel1.add(button1);
     panel1.add(button2);
